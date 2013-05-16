a89843401b7a9118b748c6b0172b9d051934d106
Lothar Rubusch <l.rubusch@gmx.ch>
2013-05-16 03:56:31 +0200

    image processing threshold

--- qt__image03_threshold/image.cpp
+++ qt__image03_threshold/image.cpp
@@ -7,13 +7,16 @@
 #include <QtCore>
 #include <QCoreApplication>
 
+#include <cstdlib>
+#include <cstdio>
+
 int main(int argc, char *argv[])
 {
         // qt settings
         QApplication app( argc, argv );
 
         // 1) read image: init png
-        QImage png_orig( "a09.png" );
+        QImage png_orig( "a09_gray.png" );
         QImage png = png_orig.convertToFormat( QImage::Format_RGB32 );
 
 
@@ -24,9 +27,13 @@ int main(int argc, char *argv[])
                         // Returns the color of the pixel at the given position.
                         int pixel = png.pixel( xcoord, ycoord );
 
+
                         //  Returns a gray value (0 to 255) from the (r, g, b) triplet.
                         //  The gray value is calculated using the formula (r * 11 + g * 16 + b * 5)/32.
                         int gray  = qGray( pixel );
+
+                        // filter out only the white pieces
+                        if( 70 > gray ){ gray = 0; }
 
                         // Returns the alpha component of the ARGB quadruplet rgba
                         int alpha = qAlpha( pixel );
@@ -51,7 +58,7 @@ int main(int argc, char *argv[])
         painter.end();
 
         // 6) save
-        png_new.save( "a09_gray.png" );
+        png_new.save( "a09_threshold_white.png" );
 
         // 5) display image
         QLabel *label = new QLabel();
