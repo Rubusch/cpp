8410bf91d0946fcba414c787dc4e88e67ed87ee9
Lothar <Lothar@Lothars-MacBook-Pro.local>
2013-05-19 01:05:44 +0200

    opencv settings

---
+++ opencv__tracking01/main.cpp
@@ -0,0 +1,18 @@
+#include <QCoreApplication>
+/*
+ * @author: Lothar Rubusch
+ * @date: 2013-may-18
+ *
+ * resources:
+ * www.aishack.in/2010/07/tracking-colored-objects-in-opencv
+ * opencv-srf.blogspot.ch/2010/09/object-detection-using-color-seperation.html
+ */
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug( "foo" );
+
+    return a.exec();
+}
