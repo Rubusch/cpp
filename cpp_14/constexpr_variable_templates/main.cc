668dc740c0f7dec569e794c19720dbf32a8d38b6
Lothar Rubusch <L.Rubusch@gmx.ch>
2015-05-13 16:52:49 +0200

    Makefiles fixed, pt.2

---
+++ cpp_14/constexpr_variable_templates/main.cc
@@ -0,0 +1,42 @@
+/*
+ * constexpr variable templates example
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+  applying the already existing template syntax to a variable.
+*/
+template<typename T>
+constexpr T pi = T(3.1415926535897932385);
+
+/*
+  use of such a variable in generic functions
+*/
+template<typename T>
+T area_of_circle_with_radius(T radius)
+{
+  return pi<T> * radius * radius;
+}
+
+/*
+  usage
+*/
+int main(int argc, char** argv)
+{
+  int radint = 7;
+  cout << "radius = " << radint << endl;
+  cout << "area of circle = \n\t"
+       << pi<int> << " * " << radint << "^2 = \n\t"
+       << area_of_circle_with_radius<int>( radint ) << endl;
+
+  double radouble = 1.23;
+  cout << "radius = " << radouble << endl;
+  cout << "area of circle = \n\t"
+       << pi<double> << " * " << radouble << "^2 = \n\t"
+       << area_of_circle_with_radius<double>( radouble ) << endl;
+
+  return 0;
+}
---
+++ make/make04_cpp11/alpha.h
@@ -0,0 +1,19 @@
+// alpha.h
+/*
+//*/
+#ifndef ALPHA
+#define ALPHA
+
+
+#include <iostream>
+using namespace std;
+#include "beta.h"
+
+class A
+{
+public:
+    A();
+    void generateB();
+};
+
+#endif
