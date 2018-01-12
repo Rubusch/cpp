3f9fffc81661721eb3d8086ddf938a44c5852dc6
Lothar Rubusch <l.rubusch@gmx.ch>
2018-01-12 18:06:46 +0100

    fix wrong allocations in code snippets

--- pointer01/Main.cpp
+++ pointer01/Main.cpp
@@ -1,27 +1,33 @@
 // Main.cpp
 /*
-
   Pointer & References
 
   There are only three different ways setting a pointer:
 
-  Definition (directly):
+  Definition:
       int* pVar = &var;
+      // pVar points to var
 
   Declaration and Initialisation (later):
-      int* pVar;
+      int* pVar=NULL;
       pVar = &var;
-  (which is basically the same as above)
-
+      // same as above
   or
-      int* pVar;
+      int* pVar=NULL;
+      // pVar here needs allocation 'pVar = new int;'
       *pVar = var;
-  (which just inits by the value)
+      // then pVar needs 'delete pVar;'
+
+  here the value is copied into some space the pointer
+  points to, thus the pointer first needs to have 'valid'
+  memory e.g. by dynamic allocation
 //*/
 
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 
 void display(string szName, string szCommand, int *p)
 {
@@ -45,8 +51,6 @@ int main()
          << ",\t&var = " << &var << endl;
     cout << endl;
 
-
-
     cout << "Deklaration and initialisation of a pointer:" << endl;
     {
         int* pVar1 = &var;
@@ -63,37 +67,41 @@ int main()
 
     cout << "Initialisation of an already declared pointer:" << endl;
     {
-        int* pVar3;
+        int* pVar3=NULL;
 //        pVar3 = var;
         cout << "pVar3 = var, Invalid conversion from \'int\' to \'int*\'" << endl;
         cout << endl;
     };
     {
-        int* pVar4;
+        int* pVar4=NULL;
         pVar4 = &var;
         display("pVar4", "pVar4 = &var", pVar4);
         cout << endl;
     };
     {
-        int* pVar5;
+        int* pVar5=NULL;
+        pVar5 = new int;       // ATTENTION! *pVar5 until now still had no memory!
         *pVar5 = var;
         display("pVar5", "*pVar5 = var", pVar5);
+        delete pVar5;          // don't forget to free the memory
         cout << endl;
+        /* without allocation of *pVar5 it even may work by chance,
+           or more likely it may throw a segmentation fault */
     };
     {
-        int* pVar6;
+        int* pVar6=NULL;
 //        *pVar6 = &var;
         cout << "*pVar6 = &var, Invalid conversion from \'int*\' to \'int\'" << endl;
         cout << endl;
     };
     {
-        int* pVar7;
+        int* pVar7=NULL;
 //        &pVar7 = var;
         cout << "&pVar7 = var, Invalid lvalue in assignment" << endl;
         cout << endl;
     };
     {
-        int* pVar8;
+        int* pVar8=NULL;
 //        &pVar8 = &var;
         cout << "&pVar8 = &var, Invalid lvalue in assignment" << endl;
         cout << endl;
--- pointer02/Main.cpp
+++ pointer02/Main.cpp
@@ -6,7 +6,8 @@
       pVar = &var;
 
   The pointer contains the same content but uses different address:
-      *pVar = var;  
+      *pVar = var;
+  This needs pVar to have allocated memory first!
 //*/
 
 #include <iostream>
@@ -16,29 +17,29 @@ using namespace std;
 int main()
 {
     cout << " - Usage of Pointers - " << endl;
-    cout << endl;    
+    cout << endl;
     {
-        cout << "The pointer shares the same address with a variable\n" 
-             << "if the variable changes the content of the pointer changes, too:" 
+        cout << "The pointer shares the same address with a variable\n"
+             << "if the variable changes the content of the pointer changes, too:"
              << endl;
 
         cout << "pVar = &var;" << endl;
         cout << endl;
-        
+
         int var = 7;
-        int* pVar; // same as: int* pVar = &var
+        int* pVar = NULL; // same as: int* pVar = &var
         pVar = &var;
-        
+
         cout << "var = " << var << ", *pVar = " << *pVar << endl;
         cout << "&var = " << &var << ", pVar = " << pVar << " (equal address)" << endl;
-        
+
         cout << "++var;" << endl;
         ++var;
-        
+
         cout << "var = " << var << ", *pVar = " << *pVar << endl;
         cout << "--*pVar;" << endl;
         --*pVar;
-        
+
         cout << "var = " << var << ", *pVar = " << *pVar << endl;
         cout << endl;
     };
@@ -49,25 +50,31 @@ int main()
 
         cout << "*pVar = var;" << endl;
         cout << endl;
-        
+
         int var = 7;
-        int* pVar;
+        int* pVar = NULL;
+
+        /* NEEDS ALLOCATION OR SEGFAULT */
+        pVar = new int;
+
         *pVar = var;
 
         cout << "var = " << var << ", *pVar = " << *pVar << endl;
         cout << "&var = " << &var << ", pVar = " << pVar << " (different address)" << endl;
-        
+
         cout << "++var;" << endl;
         ++var;
-        
+
         cout << "var = " << var << ", *pVar = " << *pVar << endl;
         cout << "--*pVar;" << endl;
         --*pVar;
-        
+
         cout << "var = " << var << ", *pVar = " << *pVar << endl;
         cout << endl;
+
+        /* FREE ALLOCATION */
+        delete pVar;
     };
-    
-    
+
     return 0;
 };
