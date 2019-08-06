dd2cb8d582ec68a031d7af71af2df7e86be21f8f
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-06 12:01:26 +0200

    move: example of move constructor and std::move()

--- 000__cpp__language-basics/move-assignment-cpp11-TODO/moveassignment.cpp
+++ 000__cpp__language-basics/move-assignment-cpp11-TODO/moveassignment.cpp
@@ -23,19 +23,95 @@ using namespace std;
 class Box
 {
 private:
-  string item;
+  string *pItem = nullptr;
+  string anotherItem;
 
 public:
-  Box(string it) : item(it) {}
+  Box(string* item)
+  : pItem(item)
+  {
+    anotherItem = string("mouse");
+  }
+
+  // copy constructor
+  Box(const Box& cpy)
+  {
+    if (this == &cpy) return;
+
+    pItem = new string(*cpy.pItem);
+    anotherItem = cpy.anotherItem;
+  }
+
+  // move constructor
+  Box(Box&& mve) noexcept
+  {
+    if (this == &mve) return;
+
+    pItem = new string(*mve.pItem);
+    delete mve.pItem; mve.pItem = nullptr;
+
+    anotherItem = mve.anotherItem;
+  }
+
+  ~Box()
+  {
+    if (pItem) delete pItem;
+  }
+
+  constexpr string& getItem() { return *pItem; }
+  constexpr string& getAnotherItem() { return anotherItem; }
+  const string isItem() { return ((nullptr == pItem) ? "NULL, getItem() would end in 'Segmentation Fault!'" : "allocated" ); }
 };
+
 
 
 int main(void)
 {
-  string item = "elephant";
-  Box box(item);
+  string *pAnimal = new string("elephant");
+  cout << "addr of pAnimal: \t\t" << pAnimal << endl << endl;
 
-  cout << "XXX: " << item << endl;
+  Box box(pAnimal);
+
+  // the box itself
+
+  cout << "addr of 'box': \t\t\t" << &box << endl;
+  cout << "addr of 'box::item': \t\t" << &box.getItem() << " <- same as pAnimal, since there allocated" << endl;
+  cout << "addr of 'box::anotherItem': \t" << &box.getAnotherItem() << endl;
+  cout << endl;
+
+  // a reference (all addresses are identical)
+  Box &referenceBox = box;
+
+  cout << "addr of 'box' reference: \t" << &referenceBox << endl;
+  cout << "addr of 'box::item': \t\t" << &referenceBox.getItem() << endl;
+  cout << "addr of 'box::anotherItem': \t" << &referenceBox.getAnotherItem() << endl;
+  cout << endl;
+
+  // a pointer
+  Box *pointerBox = &box;
+
+  cout << "addr of 'box' pointer: \t\t" << &pointerBox << " <-- pointer has a different address" << endl;
+  cout << "addr of 'box::item': \t\t" << &pointerBox->getItem() << endl;
+  cout << "addr of 'box::anotherItem': \t" << &pointerBox->getAnotherItem() << endl;
+  cout << endl;
+
+  // via copy constructor
+  Box copyBox(box);
+
+  cout << "addr of 'box' pointer: \t\t" << &copyBox << " (deep copy differs)" << endl;
+  cout << "addr of 'box::item': \t\t" << &copyBox.getItem() << " (differs)" << endl;
+  cout << "addr of 'box::anotherItem': \t" << &copyBox.getAnotherItem() << " (differs)" << endl;
+  cout << endl;
+
+  // via move
+  Box moveBox = std::move(box);
+
+  cout << "addr of 'box' pointer: \t\t" << &moveBox << " (deep copy differs)" << endl;
+  cout << "addr of 'box::item': \t\t" << &moveBox.getItem() << " (differs)" << endl;
+  cout << "addr of 'box::anotherItem': \t" << &moveBox.getAnotherItem() << " (differs)" << endl;
+  cout << endl;
+
+  cout << "addr of box::item == null? \t" << box.isItem() << endl;
 
   cout << "READY." << endl;
 
