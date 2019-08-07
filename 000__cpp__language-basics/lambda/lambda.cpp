4aae2ab5784e9487b8f45fa9d85d964041749e49
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-07 11:57:48 +0200

    lambda: some example implemented

--- 000__cpp__language-basics/lambda/lambda.cpp
+++ 000__cpp__language-basics/lambda/lambda.cpp
@@ -26,30 +26,63 @@
 
 //*/
 
+#include <vector> /* vector */
+#include <algorithm> /* remove_if() */
+#include <functional> /* lambda */
 #include <iostream>
 
 using namespace std;
 
 
-class Box
+class Greek
 {
 private:
   const string *pItem = nullptr;
 
 public:
-  Box(const string *item)
+  Greek(const string *item)
   {
-    cout << "CALLED: constructor" << endl;
-
     pItem = item;
   }
 
-  ~Box()
+  // !!! always respect rule of five having member pointer to allocated things !!!
+  //
+  // copy assignment constructor: needed for iterating on vector<Greek>
+  Greek(const Greek& gr)
   {
-    cout << "CALLED: destructor" << endl;
+    if (this == &gr) return;
+    pItem = new string(*gr.pItem);
+  }
 
+  // !!! always respect rule of five having member pointer to allocated things !!!
+  //
+  // copy move constructor: needed for vector<Greek>::push_back()
+  Greek(Greek&& gr)
+  {
+    if (this == &gr) return;
+    pItem = gr.pItem;
+    gr.pItem = nullptr;
+  }
+
+  // !!! always respect rule of five having member pointer to allocated things !!!
+  //
+  // copy move constructor: needed for vector<Greek>::push_back()
+  ~Greek()
+  {
     if (nullptr != pItem) delete pItem;
     pItem = nullptr;
+  }
+
+  // !!! always respect rule of five having member pointer to allocated things !!!
+  //
+  // needed for remove_if() and lambda
+  constexpr Greek& operator=(const Greek& gr)
+  {
+    if (this != &gr) {
+      if (pItem) delete pItem;
+      pItem = new string(*gr.pItem);
+    }
+    return *this;
   }
 
   // some getter
@@ -57,21 +90,45 @@ public:
   {
     return pItem;
   }
+
+  // print out class
+  friend
+  ostream& operator<<(ostream& out, Greek& gr);
 };
+
+ostream& operator<<(ostream& out, Greek& gr)
+{
+  if (gr.pItem) return out << *gr.pItem; // segfaults: when copy assignment ctor is missing
+  return out << "";
+}
 
 
 int main(void)
 {
-  string *pStr = new string("Perikles");
-  cout << "Address of pStr target (pointee): \t" << pStr << endl;
+  vector<Greek> box;
+  box.push_back( Greek(new string("Perikles"))); // double free: when copy move ctor missing
+  box.push_back( Greek(new string("Poplicola")));
+  box.push_back( Greek(new string("Nikias")));
+  box.push_back( Greek(new string("Themistokles")));
+
+  cout << "before:" << endl;
+  for (Greek gr : box) cout << "  " << gr;
+  cout << endl << endl;
+
+  // the lambda function passed to the remove_if()
+  string x = "Poplicola";
+  cout << "now removing '" << x << "'" << endl;
+  box.erase( remove_if( box.begin(), box.end()
+                        , [x](Greek gr){
+                            cout << "CALLED: lambda for '" << *gr.getItem() << "' " << endl;
+                            return *gr.getItem() == x;
+                          }), box.end() );
   cout << endl;
 
-  Box box(pStr);
-  cout << "Address of box: \t\t\t" << &box << endl;
-  cout << "Address of box.pItem: \t\t\t" << box.getItem() << " <-- identical with pointee" << endl;
-  cout << endl;
+  cout << "after:" << endl;
+  for (Greek gr : box) cout << "  " << gr;
+  cout << endl << endl;
 
-// TODO
   cout << "READY." << endl;
 }
 
