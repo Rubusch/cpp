0e28b8b19722130a64fadf99c72b09b05a70401d
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-17 12:05:10 +0200

    initializer: braces and parens example

--- 000__cpp__language-basics/TODO__initializer-braces-cpp11/initializer.cpp
+++ 000__cpp__language-basics/TODO__initializer-braces-cpp11/initializer.cpp
@@ -33,10 +33,35 @@
 
 #include <iostream>
 #include <vector>
+#include <atomic>
 
 using namespace std;
 
-class Widget{};
+
+class WidgetA
+{
+  private:
+  int x{ 0 };
+  int y = 0;
+//  int z(0); // ERROR! most-vexing-parse situation, must be braces
+};
+
+class WidgetB
+{
+  public:
+    WidgetB() { cout << "CALLED: WidgetB()" << endl; }
+    WidgetB(int i, bool b) { cout << "CALLED: WidgetB(int, bool)" << endl; }
+    WidgetB(int i, double d) { cout << "CALLED: WidgetB(int, double)" << endl; }
+};
+
+class WidgetC
+{
+  public:
+    WidgetC() { cout << "CALLED: WidgetC()" << endl; }
+    WidgetC(int i, bool b) { cout << "CALLED: WidgetC(int, bool)" << endl; }
+    WidgetC(int i, double d) { cout << "CALLED: WidgetC(int, double)" << endl; }
+    WidgetC(std::initializer_list<long double> il) { cout << "CALLED: WidgetC(initializer_list<long double>)" << endl; }
+};
 
 int main(void)
 {
@@ -59,24 +84,52 @@ int main(void)
   cout << endl;
 
   // most vexing parse
-  cout << "most vexing parse, init: e{} and declaration f()" << endl;
-  Widget e{}; // calls constructor with no arguments
-  Widget f(); // THIS is a function declaration, not a variable definition!!!
+  cout << "most vexing parse, initialization" << endl;
+  cout << "WidgetA e{}; - calls constructor with empty arguments" << endl;
+  WidgetA e{};
+  cout << "WidgetA f(); - a function declaration!!!" << endl;
+  WidgetA f(); // THIS is a function declaration, not a variable definition!!!
+  cout << endl;
+
+
+  // where it must be braces: uncopyable objects, e.g. atomic, must be initialized using braces
+  cout << "initialization of noncopyable objects" << endl;
+  std::atomic<int> ai1{ 0 };
+  std::atomic<int> ai2( 0 );
+//  std::atomic<int> ai3 = 0; // ERROR!
+  cout << endl;
+
+  // ATTENTION: initializer_list catches all
+  cout << "initializing an object" << endl;
+  cout << "WidgetB w1(10, true); ";
+  WidgetB w1(10, true);
+  cout << "WidgetB w2{10, true); ";
+  WidgetB w2{10, true};
+  cout << "WidgetB w3(10, 7.9); ";
+  WidgetB w3(10, 7.9);
+  cout << "WidgetB w4{10, 7.9}; ";
+  WidgetB w4{10, 7.9};
+  cout << endl;
+
+  cout << "now using initializer_list (is hijacking other constructor signatures)" << endl;
+  cout << "WidgetC w5(10, true); ";
+  WidgetC w5(10, true);
+  cout << "WidgetC w6{10, true); ";
+  WidgetC w6{10, true};
+  cout << "WidgetC w7(10, 7.9); ";
+  WidgetC w7(10, 7.9);
+  cout << "WidgetC w8{10, 7.9}; ";
+  WidgetC w8{10, 7.9};
   cout << endl;
 
   // vector special situation with braces and parens
-  cout << "special constructor calls: vector<int>" << endl;
+  cout << "example: vector<int> using initializer lists" << endl;
   vector<int> vec1{ 10, 20 };
   cout << "vec1{ 10, 20 } - vec1.size(): " << vec1.size() << endl;
 
   vector<int> vec2( 10, 20 );
   cout << "vec2( 10, 20 ) - vec2.size(): " << vec2.size() << endl;
-
   cout << endl;
-
-
-  // where it must be braces
-  
 
   cout << "READY." << endl;
 }
