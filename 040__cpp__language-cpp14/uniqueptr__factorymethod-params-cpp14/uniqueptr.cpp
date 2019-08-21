2d147151d96f1dd9ef3442d350d97a111349e83f
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-22 00:15:41 +0200

    uniqueptr: customized delete, passing params, C++14

---
+++ 040__cpp__language-cpp14/uniqueptr__factorymethod-params-cpp14/uniqueptr.cpp
@@ -0,0 +1,102 @@
+/*
+  C++11 - prefer unique_ptr to auto_ptr and raw pointers (Meyers / item 18)
+
+
+  // factory method:
+  // return std::unique_ptr to an object created from the given args
+
+  template< typename... Ts>
+  std::unique_ptr< Fruit >
+  makeJuice(Ts&&... params);
+
+
+ Conclusion:
+
+ - 'std::unique_ptr' is a small, fast, move-only smart pointer for managing
+   resources with exclusive-ownership semantics
+
+ - by default, resource destruction takes place via 'delete', but custom
+   deleters can be specified. Stateful deleters and function pointers as
+   deleters increase the size of 'std::unique_ptr' objects
+
+ - converting a 'std::unique_ptr' to a 'std::shared_ptr' is easy
+
+ @author: lothar Rubusch
+ */
+
+#include <iostream>
+#include <memory> /* unique_ptr */
+
+using namespace std;
+
+
+static auto is_orange_juice=true;
+
+class Fruit
+{
+public:
+  Fruit()
+  {
+    cout << "CALLED: Fruit()" << endl;
+  }
+  virtual ~Fruit() = default; // essential: derrived class object shall be destructed via base class pointer
+};
+
+
+class Lemon
+  : public Fruit
+{
+public:
+  Lemon()
+  {
+    cout << "CALLED: Lemon()" << endl;
+  }
+};
+
+
+class Orange
+  : public Fruit
+{
+public:
+  Orange()
+  {
+    cout << "CALLED: Orange()" << endl;
+  }
+};
+
+
+// customize delete, e.g. logs out
+auto delete_fruit = [](Fruit* pFruit)
+  {
+    cout << "CALLED: delete_fruit" << endl;
+    delete pFruit;
+  };
+
+
+
+// revised return type, version with customized delete
+
+// C++14: move params over to pointer
+template< typename... Ts>
+auto makeJuice(Ts&&... params)
+{
+  // pointer to be returned
+  unique_ptr< Fruit, decltype(delete_fruit) > pFruit(nullptr, delete_fruit);
+
+  if (is_orange_juice) {
+    pFruit.reset(new Orange(std::forward< Ts >(params)...));
+  } else {
+    pFruit.reset(new Lemon(std::forward< Ts >(params)...));
+  }
+  return pFruit;
+}
+
+
+int main(void)
+{
+  cout << "let's get some fresh fruit" << endl;
+  auto pointerjuice = makeJuice();
+
+  // work with the pointerjuice, at exit of scope it's deleted automatically
+  cout << "READY." << endl;
+}
