9083bfcfd541be013172427c0f29cd8bedf9b548
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-22 23:21:09 +0200

    uniqueptr: continued

--- 040__cpp__language-cpp14/uniqueptr/uniqueptr.cpp
+++ 040__cpp__language-cpp14/uniqueptr/uniqueptr.cpp
@@ -1,6 +1,10 @@
 /*
   C++11 - prefer unique_ptr to auto_ptr and raw pointers (Meyers / item 18)
 
+  unique_ptr is always the unique owner of the raw pointer resource!
+  thus a unique_ptr object cannot be copied - only moved (i.e. the moving
+  pointer looses its ownership, can mean it will be destroyed after moving)
+
   demonstrates basic operations with unique_ptr
 
  @author: lothar Rubusch
@@ -37,10 +41,29 @@ public:
 
 int main(void)
 {
+  // creating a pointer and initialization
   std::unique_ptr< Box > pBox( new Box(12) );
 
+  // creation of an empty pointer is possible
+  std::unique_ptr< Box > pAnotherBox;
+
+  // check if a pointer is empty
+  if (!pAnotherBox) {
+    cout << "there is no other box" << endl;
+  }
+
+  // check if a pointer is empty
   auto content = pBox->box_content();
-  cout << "The box contains " << content << " toys" << endl;
+  cout << "the box contains " << content << " toys" << endl;
+
+  // reset a unique_ptr
+  pBox.reset();
+
+  if (nullptr == pBox) {
+    cout << "the box disappeared...." << endl;
+  }
 
   cout << "READY." << endl;
+
+  return EXIT_SUCCESS;
 }
