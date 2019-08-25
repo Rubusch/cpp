7d8842eeeef4217f637fd9ea37af530f193d2292
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-25 22:41:36 +0200

    uniqueptr: resource in comment

--- 040__cpp__language-cpp14/uniqueptr__factorymethod-params-cpp14/uniqueptr.cpp
+++ 040__cpp__language-cpp14/uniqueptr__factorymethod-params-cpp14/uniqueptr.cpp
@@ -20,6 +20,7 @@
    deleters increase the size of 'std::unique_ptr' objects
 
  - converting a 'std::unique_ptr' to a 'std::shared_ptr' is easy
+  resources: Effective Modern C++, Scott Meyers, 2015
 
  @author: lothar Rubusch
  */
