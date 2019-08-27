f3150c5a4793878eb4a7844cfa9d5a6d8df801d8
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-27 17:44:48 +0200

    string-to-int-to-string: added

--- 010__cpp__language-techniques/number_to_string_to_number/conversion.cpp
+++ 010__cpp__language-techniques/number_to_string_to_number/conversion.cpp
@@ -3,9 +3,9 @@
   simple conversion
 //*/
 
-
 #include <iostream>
-
+#include <sstream> /* C++98 style */
+#include <string>
 
 using namespace std;
 
@@ -15,7 +15,45 @@ using namespace std;
 //*/
 int main()
 {
-  // TODO
+  // string to int
+  string szNumber("777");
+  int iNumber = 0;
+
+  // C++98
+  cout << "C++98: string (" << szNumber << ") -> number (" << iNumber << ")\n";
+  std::istringstream iss(szNumber);
+  iss >> iNumber;
+  if (iss.fail()) cerr << "Failed!" << endl;
+  cout << "result: number " << iNumber << endl;
+  cout << endl;
+
+  // C++11
+  szNumber = "777";
+  iNumber = 0;
+  cout << "C++11: string (" << szNumber << ") -> number (" << iNumber << ")\n";
+  iNumber = std::stoi(szNumber);
+  cout << "result: number " << iNumber << endl;
+  cout << endl;
+
+  // int to string
+  // C++98
+  szNumber = "";
+  iNumber = 777;
+  cout << "C++98: number (" << iNumber << ") -> string (" << szNumber << ")\n";
+  std::stringstream ss;
+  ss << iNumber;
+  szNumber = ss.str();
+  cout << "result: string '" << szNumber << "'" << endl;
+  cout << endl;
+
+  // C++11
+  szNumber = "";
+  iNumber = 777;
+  cout << "C++11: number (" << iNumber << ") -> string (" << szNumber << ")\n";
+  szNumber = std::to_string(iNumber);
+  cout << "result: string '" << szNumber << "'" << endl;
+  cout << endl;
+
   cout << "READY.\n";
   return 0;
 }
