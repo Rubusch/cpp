01ce8f63126387556aafdbf41b6bd9e3bf1e0a85
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-27 17:31:46 +0200

    number-to-string-to-number: added

---
+++ 010__cpp__language-techniques/number_to_string_to_number/conversion.cpp
@@ -0,0 +1,21 @@
+// number to string to number
+/*
+  simple conversion
+//*/
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+/*
+  main..
+//*/
+int main()
+{
+  // TODO
+  cout << "READY.\n";
+  return 0;
+}
