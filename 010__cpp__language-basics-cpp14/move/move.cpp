d35496d36cba4dd7692e3cc96247c762fdb8e41d
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-06 16:24:30 +0200

    move: started

---
+++ 010__cpp__language-basics-cpp14/move/move.cpp
@@ -0,0 +1,30 @@
+/*
+  C++11 - rvalue reference, move semantics and perfect forwarding (Meyers / item 23)
+
+  ---
+
+  lvalue    - has an address, e.g. a user maintained variable or memory with
+              accessible address
+
+  rvalue    - has no accessible address, e.g. a plain number value (stored only
+              temporarlily)
+
+
+  conclusion:
+
+  // TODO
+
+  resources: Effective Modern C++, Scott Meyers, 2015
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+  // TODO
+
+  cout << "READY." << endl;
+}
