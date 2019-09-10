d3ad3751649579b482bfe8d3542a2df02eb66f8c
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-10 16:13:25 +0200

    callback: update by tutorial

--- 010__cpp__language-basics-cpp14/callbacks/callback.cpp
+++ 010__cpp__language-basics-cpp14/callbacks/callback.cpp
@@ -8,13 +8,39 @@
  */
 
 #include <iostream>
+#include <cstdlib>
+#include <math.h>
 
 using namespace std;
 
 
-int main(void)
-{
-  cout << "TODO" << endl;
+// TODO           
+
+class SquareRoot {
+public:
+  // main logic of class.
+  double run(const double input) {
+    if (input < 0.0) throw 0; // error checking.
+    this->iteration = 0;      // reset iteration number.
+    double guess = input;     // set initial guess to input.
+    // heron algorithm
+    while (std::fabs(guess - input/guess) > this->epsilon) {
+      guess = (guess + input / guess) / 2.0;
+      ++iteration;
+    }
+    return guess;
+  }
+
+private:
+  const double epsilon = 1e-6; // maximum allowed error.
+  size_t iteration = 0;        // iteration number.
+};
+
+
+int main() {
+  SquareRoot p;
+  std::cout << "Result: " << p.run(1234.5*1234.5) << std::endl;
 
   cout << "READY." << endl;
+  return EXIT_SUCCESS;
 }
