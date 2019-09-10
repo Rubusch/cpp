8114f80c1bdd68ca580761bbe2fdb7fff6a513e8
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-10 12:30:06 +0200

    bitfield: update

--- 010__cpp__language-basics-cpp14/bitfield/bitfield.cpp
+++ 010__cpp__language-basics-cpp14/bitfield/bitfield.cpp
@@ -1,6 +1,16 @@
 /*
   bitfields experienced
 
+  - pointers or non-const references are not possible
+
+  - the type of a bit field can only be integral or enumeration type
+
+  - cannot be a static member
+
+  - no pure rvalues, rvalue-to-lvalue conversion
+
+  - default initialized to '0'
+
   this should be a bitfield experiencing ground, but Scott Meyers mentiones
   bitfields in one item, so it's refered here, to be kept in mind
 
@@ -36,6 +46,7 @@ struct BitField
 struct DerivedBitField
 : public BitField
 {
+// if the specified size of the bit field is greater than the size of its type, the value is limited by the type:
   unsigned int another_bits : 2;
 };
 
@@ -59,14 +70,32 @@ int main(void)
   cout << "bf.bits: " << bf.bits << endl;
   cout << endl;
 
-// TODO sizes
+  cout << "sizeof(bf): " << sizeof(bf) << endl
+       << "sizeof(bf.bits): ERROR - can't apply sizeof to a bitfield" << endl;
+  cout << "&bf: " << &bf << endl
+       << "&bf.bits: ERROR - can't get address of a bitfield" << endl;
+  cout << endl;
 
-    // initialization with '6'
+  // derived bitfield
   cout << "derived bitfield dbf created" << endl;
   DerivedBitField dbf = DerivedBitField();
   dbf.bits = 7;
   dbf.another_bits = 2;
   cout << "dbf.bits: " << dbf.bits << ", dbf.another_bits: " << dbf.another_bits << endl;
+  cout << endl;
+
+  cout << "another derived bitfield adbf created (only one initialization)" << endl;
+  DerivedBitField adbf = DerivedBitField();
+  adbf.bits = 7;
+  cout << "adbf.bits: " << adbf.bits << ", adbf.another_bits: " << adbf.another_bits << endl;
+  cout << endl;
+
+// note:
+//  printf("bf: 0x%lX", (unsigned long)bf); // ERROR, not possible to read out bits directly
+
+  cout << "sizeof(bf): " << sizeof(bf) << endl;
+  cout << "sizeof(dbf): " << sizeof(dbf) << endl;
+  cout << "sizeof(adbf): " << sizeof(adbf) << endl;
   cout << endl;
 
   cout << "READY." << endl;
