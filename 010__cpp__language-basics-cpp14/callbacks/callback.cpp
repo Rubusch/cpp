1fd571d5b9860b89d3e39d823165bd0df6b93a25
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-10 10:54:55 +0200

    callbacks: started

---
+++ 010__cpp__language-basics-cpp14/callbacks/callback.cpp
@@ -0,0 +1,20 @@
+/*
+  callbacks in cpp
+
+  resources:
+  https://sudomakeinstall.io/posts/2017/11/30/callbacks-in-cpp11/ , blog post, 2017
+  Effective Modern C++, Scott Meyers, 2015
+  cppreference.com, 2019
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+  // TODO
+
+  cout << "READY." << endl;
+}
