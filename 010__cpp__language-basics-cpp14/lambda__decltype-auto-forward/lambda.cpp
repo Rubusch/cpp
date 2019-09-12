555979d37fea98b2b0dede516b4e13e45ff18f41
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-12 23:45:39 +0200

    lambda: decltype and auto example added

---
+++ 010__cpp__language-basics-cpp14/lambda__decltype-auto-forward/lambda.cpp
@@ -0,0 +1,95 @@
+// lambda.cpp
+/*
+  Lambda (>=cpp11) constructs a closure: an unnamed function object capable of capturing variables in scope.
+
+  This example shows to use 'decltype' on 'auto&&' paramters for 'std::forward'.
+
+
+  ---
+
+
+  fundamentals
+  ------------
+
+  int x = 10;
+  (...)
+  auto var = [x] (int y) constexpr -> int {return x * y > 123; };
+
+
+
+  syntax variants:
+
+  [capture] (params) mutable exception attribute -> ret { body }
+  [capture] (params) -> ret { body }
+  [capture] (params) { body }
+  [capture] { body }
+
+
+
+  terminology:
+
+  auto var : closure
+    the lambda object is called 'closure'
+
+  [x] : capture
+    which symbols are captured and thus visible for the lambda body, e.g.
+    [a,&b]   a as by value copy, b as reference
+    [this]   this pointer as a by value copy
+    [&]      by-reference: all symbols available in function scope as reference
+    [=]      by-value: all symbols available in function scope as a copy
+    []       nothing captured
+    CAUTION: references can be dangling references!
+
+  (int y) : params
+    list of parameters
+
+  constexpr -> int : (optional, can be left out)
+    specifier: mutable, constexpr, consteval (opt)
+    where:
+      mutable = allows the body to modify copied params and to call their non-const member functions
+      exception = exception specification, or noexcept
+      attribute = attribute specificatio for the closure type
+
+    return type or use auto for closure, defaults to "void" (opt)
+
+  {return x * y > 123; }; : function body
+    implementation of the lambda class
+
+
+
+  C++11 - use 'decltype' on 'auto&&' parameters to 'std::forward' them
+  (Meyers / item 33)
+
+  CONCLUSION
+
+  - Use 'decltype' on 'auto&&' parameters to 'std::forward' them.
+
+  resources:
+  Effective Modern C++, Scott Meyers, 2015
+  cppreference.com, 2019
+//*/
+
+#include <iostream>
+
+using namespace std;
+
+//*
+auto closure = [](auto par){ return (par % 3); };
+/*/ generates something like this
+class CompilerGeneratedClosureClassName
+{
+public:
+  template< typename T >
+  auto operator()(T par) const
+  { return (par % 3); }
+};
+auto closure = CompilerGeneratedClosureClassName();
+// */
+
+int main(void)
+{
+  for (auto idx=0; idx<10; ++idx) {
+    cout << "item(" << idx << ") : " << closure(idx) << endl;
+  }
+  cout << "READY." << endl;
+}
