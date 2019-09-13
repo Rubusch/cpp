00660628bea5433ba19fb8507509bb6dbbca519a
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-13 23:36:04 +0200

    bind: example started

---
+++ 010__cpp__language-basics-cpp14/bind/bind.cpp
@@ -0,0 +1,38 @@
+// bind.cpp
+/*
+  bind
+
+  demostrates the usage of bind (C++11)
+
+  ---
+
+
+  C++14 - prefer lambdas to std::bind
+  (Meyers / item 34)
+
+
+  CONCLUSION
+
+  - Lambdas are more readable, more expressive, and may be more efficient than
+    using 'std::bind'.
+
+  - In C++11 only, 'std::bind' may be useful for implementing move capture or
+    for binding objects with templatized function call operators.
+
+  resources:
+  Effective Modern C++, Scott Meyers, 2015
+  cppreference.com, 2019
+//*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+  // TODO
+
+  cout << "READY." << endl;
+}
+
