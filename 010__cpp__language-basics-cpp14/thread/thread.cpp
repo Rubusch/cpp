462617bf2b0e2cf179779df48bb7397a712768b6
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-15 18:58:41 +0200

    thread: formatting and comments

--- 010__cpp__language-basics-cpp14/thread/thread.cpp
+++ 010__cpp__language-basics-cpp14/thread/thread.cpp
@@ -1,31 +1,27 @@
 /*
   C++11 - make 'const' member functions thread safe (Meyers / item 16)
 
-  ---
-
-  lvalue    - has an address, e.g. a user maintained variable or memory with
-              accessible address
-
-  rvalue    - has no accessible address, e.g. a plain number value (stored only
-              temporarlily)
 
 
-  conclusion:
+  CONCLUSION
 
-  - make 'const' member functions thread safe unless you're certain they'll
-    never (!) be used in a concurrent context
+  * Make 'const' member functions thread safe unless you're certain they'll
+    never (!) be used in a concurrent context.
 
-  - use of 'std::atomic' variables may offer better performance than a mutex,
+  * Use of 'std::atomic' variables may offer better performance than a mutex,
     but they're suited for manipulation of only a single variable or memory
-    location
+    location.
 
-  - for a single variable or memory location requiring synchronization, use
+  * For a single variable or memory location requiring synchronization, use
     of a 'std::atomic' is adequate, but once you get to two or more variables
     or memory locations that require manipulations as a unit, you should
-    reach for a mutex
+    reach for a mutex.
 
-  @author: Lothar Rubusch
-  resources: Effective Modern C++, Scott Meyers, 2015
+
+
+  RESOURCES
+
+  * Effective Modern C++, Scott Meyers, 2015
  */
 
 #include <iostream>
@@ -68,4 +64,3 @@ int main(void)
   cout << "READY." << endl;
   return 0;
 }
-
