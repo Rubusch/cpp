a955c82850d4c80a557d3e3c211719c410f5ad7c
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-15 19:17:36 +0200

    thread: formatting in async example

--- 010__cpp__language-basics-cpp14/thread__async-TODO/async.cpp
+++ 010__cpp__language-basics-cpp14/thread__async-TODO/async.cpp
@@ -39,22 +39,27 @@
 #include <string>
 #include <mutex>
 
+using namespace std;
+
+
 std::mutex m;
 
 struct X {
   void foo(int i, const std::string& str) {
-    std::lock_guard<std::mutex> lk(m);
-    std::cout << str << ' ' << i << '\n';
+    std::lock_guard< std::mutex > lk(m);
+    cout << str << ' ' << i << '\n';
   }
+
   void bar(const std::string& str) {
-    std::lock_guard<std::mutex> lk(m);
-    std::cout << str << '\n';
+    std::lock_guard< std::mutex > lk(m);
+    cout << str << '\n';
   }
+
   int operator()(int i) {
-        std::lock_guard<std::mutex> lk(m);
-        std::cout << i << '\n';
-        return i + 10;
-    }
+    std::lock_guard< std::mutex > lk(m);
+    cout << i << '\n';
+    return i + 10;
+  }
 };
 
 
@@ -62,13 +67,16 @@ template <typename RandomIt>
 int parallel_sum(RandomIt beg, RandomIt end)
 {
   auto len = end - beg;
-  if (len < 1000)
-    return std::accumulate(beg, end, 0);
+  if (len < 1000) return std::accumulate(beg, end, 0);
 
   RandomIt mid = beg + len/2;
-  auto handle = std::async(std::launch::async,
-                             parallel_sum<RandomIt>, mid, end);
+
+  // call itself as 'std::async()' in parallel recursively
+  auto handle = std::async(std::launch::async, parallel_sum< RandomIt >, mid, end);
+
+  // call itself recursively
   int sum = parallel_sum(beg, mid);
+
   return sum + handle.get();
 }
 
@@ -76,11 +84,11 @@ int parallel_sum(RandomIt beg, RandomIt end)
 int main()
 {
   std::vector<int> v(10000, 1);
-  std::cout << "The sum is " << parallel_sum(v.begin(), v.end()) << '\n';
+  cout << "The sum is " << parallel_sum(v.begin(), v.end()) << '\n';
 
   X x;
-  // Calls (&x)->foo(42, "Hello") with default policy:
-  // may print "Hello 42" concurrently or defer execution
+  // calls (&x)->foo(42, "Hello") with default policy:
+  //   may print "Hello 42" concurrently or defer execution
   auto a1 = std::async(&X::foo, &x, 42, "Hello");
   // Calls x.bar("world!") with deferred policy
   // prints "world!" when a2.get() or a2.wait() is called
