582082c61c978f76c5f17f67de7f370e1e18ae4c
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-16 11:01:47 +0200

    thread: future example, ongoing

--- 010__cpp__language-basics-cpp14/thread__future/future.cpp
+++ 010__cpp__language-basics-cpp14/thread__future/future.cpp
@@ -1,7 +1,7 @@
 /*
   C++11 - prefer task-based programming to thread-based (Meyers / item 35)
 
-
+// TODO                     
 
   CONCLUSION
 
@@ -19,6 +19,36 @@
   cppreference.com, 2019
  */
 
+/*
+#include <iostream>
+#include <future>
+#include <thread>
+
+int main()
+{
+  // future from a packaged_task
+  std::packaged_task<int()> task([]{ return 7; }); // wrap the function
+  std::future<int> f1 = task.get_future();  // get a future
+  std::thread t(std::move(task)); // launch on a thread
+
+  // future from an async()
+  std::future<int> f2 = std::async(std::launch::async, []{ return 8; });
+
+  // future from a promise
+  std::promise<int> p;
+  std::future<int> f3 = p.get_future();
+  std::thread( [&p]{ p.set_value_at_thread_exit(9); }).detach();
+
+  cout << "Waiting..." << std::flush;
+  f1.wait();
+  f2.wait();
+  f3.wait();
+  cout << "Done!\nResults are: "
+       << f1.get() << ' ' << f2.get() << ' ' << f3.get() << '\n';
+  t.join();
+}
+// */
+
 #include <iostream>
 #include <thread> /* thread, this_thread::sleep_for() */
 #include <future>
@@ -28,34 +58,27 @@ using namespace std;
 
 int doAsyncWork()
 {
-  for (auto cnt=0; cnt<10; ++cnt) {
+  int cnt=0;
+  for (cnt=0; cnt<10; ++cnt) {
     cout << cnt << endl;
     this_thread::sleep_for(chrono::seconds(1));
   }
-  return 0;
+  return cnt;
 }
 
 
 int main(void)
 {
   cout << "future from a packaged task" << endl;
-//*
-  std::packaged_task< int() > task( [](){ return 7; } );
-/*/
-  // FIXME
-  std::packaged_task< void() > task( [](){ doAsyncWork(); } );
-// */
+  // set up a packaged_task with a lambda, then obtain the future out of the task
+  std::packaged_task< int() > task( [](){ return doAsyncWork(); } );
   std::future< int > future_from_task = task.get_future();
+  // move the task into a separate thread, to set it "join()" later
   std::thread thr( std::move(task) );
   cout << endl;
 
   cout << "future from an async()" << endl;
-//*
-  std::future< int > future_from_async = std::async( std::launch::async, [](){ return 8; } );
-/*/
-  // FIXME
-  std::future< void > future_from_async = std::async( std::launch::async, [](){ doAsyncWork(); } );
-// */
+  std::future< int > future_from_async = std::async( std::launch::async, [](){ return doAsyncWork(); } );
   cout << endl;
 
   cout << "future from a promise" << endl;
@@ -78,7 +101,8 @@ int main(void)
        << future_from_async.get() << ' '
        << future_from_promise.get() << endl;
 
-  thr.join(); // ERROR if this is missing: 'terminate called without an active exception'
+  // join the first task's thread, this will actually start the thread
+  thr.join(); // if this is missing: 'ERROR terminate called without an active exception'
 
   cout << "READY." << endl;
   return EXIT_SUCCESS;
