2e7448aeb22e254d8d93dde3d60811636e48e251
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-16 09:03:03 +0200

    thread: packed_task update

--- 010__cpp__language-basics-cpp14/thread__packed-task_TODO/task.cpp
+++ 010__cpp__language-basics-cpp14/thread__packed-task_TODO/task.cpp
@@ -1,9 +1,64 @@
 /*
   C++11 - prefer task-based programming to thread-based (Meyers / item 35)
 
+  C++11 - specify 'std::launch::async' if asynchronicity is essential
+          thread__async-launch-policies  (Meyers / item 36)
+
+
+
+  The 'std::async' creates a task, that may be parallelized by the compiler to
+  asynchronous threads.
+
+  std::launch::async - start asynchronously
+
+  std::launch::deferred - start later, at 'wait()'
+
+  The template function async runs the function f asynchronously (potentially in
+  a separate thread which may be part of a thread pool) and returns a
+  std::future that will eventually hold the result of that function call.
+
+  The implementation may extend the behavior of the first overload of std::async
+  by enabling additional (implementation-defined) bits in the default launch
+  policy.
+
+
+
+  THEORY
+
+  The three meanings of 'thread' in concurrent C++ software:
+
+  * Hardware Threads: are the threads that actually perform computation.
+    Contemporary machine architectures offer one or more hardware threads per
+    CPU core.
+
+  * Software Threads: (also known as OS threads or system threads) are the
+    threads that the operating system manages across all processes and schedules
+    for execution on hardware threads. It's typically possible to create more
+    software threads than hardware threads, because when a software thread is
+    blocked (e.g. on I/O or waiting for a mutex or condition variable),
+    throughput can be improved by executing other, unblocked threads.
+
+  * std::threads are objects in a C++ process that act as handles to underlying
+    software threads. Some 'std::thread' objects represent "null" handles, i.e.,
+    correspond to no software thread, because they're in a default-constructed
+    state (hence have no function to execute), have been moved from (the
+    moved-to 'std::thread' then acts as the handle to the underlying software
+    thread), have been joined (the function they were to run has finished), or
+    have been detached (the connection between them and their underlying
+    software thread has been severed).
+
 
 
   CONCLUSION
+
+  * The default launch policy for 'std::async' permits both asynchronous and
+    synchronous task execution.
+
+  * This flexibility leads to uncertainty when accessing 'thread_locals' (TLS,
+    Thread Local Storage), implies that the task may never execute, and affects
+    program logic for timeout-based wait calls.
+
+  * Specify 'std::launch::async' if asynchronous task execution is essential.
 
   * The 'std::thread' API offers no direct way to get return values from
     asynchronously run functions, and if those functions throw, the program is
@@ -15,18 +70,64 @@
   * Task-based programming via 'std::async' with the default launch policy
     handles most of these issues for you.
 
-  resources: Effective Modern C++, Scott Meyers, 2015
-  cppreference.com, 2019
+
+
+  RESOURCES
+
+  * Effective Modern C++, Scott Meyers, 2015
+
+  * cppreference.com, 2019
  */
 
 #include <iostream>
+#include <cmath>
+#include <thread>
+#include <future>
+#include <functional>
 
 using namespace std;
 
 
-int main(void)
+// unique function to avoid disambiguating the std::pow overload set
+int f(int x, int y) { return std::pow(x,y); }
+
+void task_lambda()
 {
-  // TODO
+  std::packaged_task< int(int, int) > task( [](int a, int b) { return std::pow(a, b); } );
+  std::future< int > result = task.get_future();
+
+  task(2, 9);
+
+  cout << "task_lambda:\t" << result.get() << '\n';
+}
+
+void task_bind()
+{
+  std::packaged_task<int()> task(std::bind(f, 2, 11));
+  std::future<int> result = task.get_future();
+
+  task();
+
+  cout << "task_bind:\t" << result.get() << '\n';
+}
+
+void task_thread()
+{
+  std::packaged_task<int(int,int)> task(f);
+  std::future<int> result = task.get_future();
+
+  std::thread task_td(std::move(task), 2, 10);
+  task_td.join();
+
+  cout << "task_thread:\t" << result.get() << '\n';
+}
+
+
+int main()
+{
+  task_lambda();
+  task_bind();
+  task_thread();
 
   cout << "READY." << endl;
   return EXIT_SUCCESS;
