fe24d0066d5578c44600f59d234d82afd8dcfd4a
Lothar Rubusch <l.rubusch@gmail.com>
2023-02-03 12:50:21 +0000

    notes: relations
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- notes/oop__association-aggregation-composition/association-aggregation-composition.cpp
+++ notes/oop__association-aggregation-composition/association-aggregation-composition.cpp
@@ -1,17 +1,59 @@
 /*
  * SNIPPET
  *
- * source:
+ * REFERENCES:
+ * https://www.visual-paradigm.com/guide/uml-unified-modeling-language/uml-aggregation-vs-composition/
  * https://softwareengineering.stackexchange.com/questions/255973/c-association-aggregation-and-composition
  *
  *
  *
+ * +---------+    association               +---------+
+ * | class 1 |----------------------------->| class 2 |
+ * +---------+                              +---------+
+ * |         |                              |         |
+ * +---------+                              +---------+
+ *
  * Association: Foo has a pointer to Bar object as a data member
+ * -> "owners feed pets, pets please owners"
+ *
+ *
+ *
+ * +---------+    aggregation               +---------+
+ * | class 1 |< >-------------------------->| class 2 |
+ * +---------+                              +---------+
+ * |         |                              |         |
+ * +---------+                              +---------+
  *
  * Aggregation: Foo has a pointer to Bar object and data of Bar is deep copied
  * in that pointer.
+ * -> "a tail is a part of both dogs and cats" (aggregation or composition)
+ *
+ *
+ * +---------+    composition               +---------+
+ * | class 1 |<X>-------------------------->| class 2 |
+ * +---------+                              +---------+
+ * |         |                              |         |
+ * +---------+                              +---------+
  *
  * Composition: Foo has a Bar object as data member.
+ *
+ *
+ * +---------+
+ * | class 1 |
+ * +---------+
+ * |         |
+ * +---------+
+ *     /_\
+ *      |
+ *      |
+ * +---------+
+ * | class 1 |
+ * +---------+
+ * |         |
+ * +---------+
+ *
+ * Inheritance:
+ * -> "a cat is a kind of pet" (inheritance / generalization)
  */
 
 
